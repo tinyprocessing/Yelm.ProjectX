197983997fb9ad584414839249253d959b59cf10
tinyprocessing <safir@thenoco.co>
2021-01-10 19:03:04 +0300

    cache / search

--- Yelm.ProjectX/extensions/view.swift
+++ Yelm.ProjectX/extensions/view.swift
@@ -75,3 +75,63 @@ private func alertViewSettingsOpen(title: String, message: String) -> UIAlertCon
     return alert
 
 }
+
+
+
+struct SearchBar: View {
+    @Binding var text: String
+
+    @State var becomeFirstResponder = false
+    @State private var isEditing = false
+    @State var TitleString: String = "Поиск ..."
+    @State var ImageString: String = "magnifyingglass"
+
+    var body: some View {
+        HStack {
+
+            TextField(TitleString, text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: ImageString)
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
+                .padding(.horizontal, 10)
+              
+
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    self.text = ""
+
+                    // Dismiss the keyboard
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }) {
+                    Text("Отмена")
+                        .font(.system(size: 20, weight: .medium, design: .rounded))
+                        .foregroundColor(Color.black)
+                }
+                    .padding(.trailing, 10)
+                    .transition(.move(edge: .trailing))
+                    .animation(.none)
+            }
+        } .padding([.leading, .trailing], 18)
+    }
+}
---
+++ Yelm.ProjectX/pages/search.swift
@@ -0,0 +1,101 @@
+//
+//  search.swift
+//  Yelm.ProjectX
+//
+//  Created by Michael on 10.01.2021.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+
+struct Search: View {
+    
+    
+
+    @ObservedObject var bottom: bottom = GlobalBottom
+    @ObservedObject var realm: RealmControl = GlobalRealm
+    @ObservedObject var search_server : search = GlobalSearch
+
+    @State var nav_bar_hide: Bool = true
+    
+    
+    @Environment(\.presentationMode) var presentation
+    
+    @State var search : String = ""
+    var body: some View {
+        VStack{
+            
+            VStack{
+                
+                    HStack(alignment: .center){
+                        Button(action: {
+                            
+                                self.presentation.wrappedValue.dismiss()
+                                let generator = UIImpactFeedbackGenerator(style: .soft)
+                                generator.impactOccurred()
+                         
+                        }) {
+
+                            Image(systemName: "arrow.backward")
+                                .foregroundColor(Color.white)
+                                .frame(width: 15, height: 15, alignment: .center)
+                                .padding([.top, .leading, .bottom, .trailing], 10)
+
+                                .font(.system(size: 15, weight: .bold, design: .rounded))
+
+                                .background(Color.blue)
+                                .clipShape(Circle())
+
+                        }
+                            .padding(.top, 10)
+                            .padding(.trailing, 10)
+                            .buttonStyle(ScaleButtonStyle())
+                        
+                        Text("Поиск товаров")
+                            .padding(.top, 10)
+                            .font(.system(size: 28, weight: .semibold, design: .rounded))
+                         
+                        
+                        Spacer()
+                    }
+                    
+            }
+            .padding([.trailing, .leading], 20)
+            .padding(.bottom, 10)
+            .clipShape(CustomShape(corner: [.bottomLeft, .bottomRight], radii: 20))
+            .shadow(color: .dropShadow, radius: 15, x: 10, y: 10)
+            .shadow(color: .dropLight, radius: 15, x: -10, y: -10)
+
+           
+                
+            SearchBar(text: $search)
+                .padding(.bottom)
+                .padding(.horizontal, -10)
+            
+            
+            ScrollView(showsIndicators: false){
+                
+                ForEach(self.search_server.items.filter { $0.title.lowercased().contains(search.lowercased()) || search.isEmpty || $0.title.lowercased().contains(search.lowercased())}, id: \.self) { tag in
+                    SearchItem(title: tag.title, image: tag.thubnail, price: tag.price, type: tag.type, quanity: tag.quanity)
+                }
+
+            }.frame(width: UIScreen.main.bounds.size.width-30)
+            
+            Spacer()
+        }
+        
+        .navigationBarTitle("hidden_layer")
+        .navigationBarHidden(self.nav_bar_hide)
+        
+        .onAppear {
+            self.nav_bar_hide = true
+            self.bottom.hide = true
+        }
+        
+        .onDisappear{
+            self.bottom.hide = false
+        }
+    }
+}
