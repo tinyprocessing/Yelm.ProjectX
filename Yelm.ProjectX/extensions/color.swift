c430e6ff4adb531a78affe050e36e9b976bd6500
Michael Safir <admin@yelm.io>
2021-01-24 02:02:02 +0300

    payment page

--- Yelm.ProjectX/extensions/color.swift
+++ Yelm.ProjectX/extensions/color.swift
@@ -13,8 +13,8 @@ extension Color {
     static let neuBackground = Color("Background")
     static let dropShadow = Color("Dark")
     static let dropLight = Color("Light")
-    static var theme = Color.init(hex: "F1C40F")
-    static var theme_foreground = Color.black
+    static var theme = Color.blue
+    static var theme_foreground = Color.white
 }
 
 extension Color {
