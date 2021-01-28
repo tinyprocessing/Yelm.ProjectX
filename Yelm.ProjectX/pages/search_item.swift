961e6ebcb70da85c64645288727b030681d10fa3
tinyprocessing <safir@thenoco.co>
2021-01-29 00:01:57 +0300

    add search discount

--- Yelm.ProjectX/pages/search_item.swift
+++ Yelm.ProjectX/pages/search_item.swift
@@ -14,41 +14,67 @@ import Yelm_Server
 struct SearchItem : View {
     
     
-    @State var title : String
-    @State var image : String
-    @State var price : String
-    @State var type : String
-    @State var quanity : String
+    @State var item : items_structure = items_structure()
     
-     var body: some View {
+    
+    var body: some View {
         VStack{
             HStack(spacing: 0){
                 
                 
-                URLImage(URL(string: image)!) { proxy in
-                      proxy.image
+                URLImage(URL(string: self.item.thubnail)!) { proxy in
+                    proxy.image
                         .resizable()
                         .frame(width: 50, height: 50)
                         .cornerRadius(8)
                 }
                 
-                Text(title)
-                    .lineLimit(2)
-                    .padding(.leading, 10)
+                VStack{
+                 
+                    
+             
+                        VStack(alignment: .leading) {
+                            
+                            Text(self.item.title)
+                                .padding(.bottom, self.item.discount_present == "-0%" ? 5 : 0)
+                            
+                            if (self.item.discount_present != "-0%"){
+                            Text("\(self.item.discount_present)")
+                                .foregroundColor(Color.white)
+                                .padding([.top, .leading, .bottom, .trailing], 7)
+                                .font(.system(size: 13, weight: .bold, design: .rounded))
+                                .background(Color.green)
+                                .clipShape(Capsule())
+                                .shadow(color: .dropShadow, radius: 15, x: 10, y: 10)
+                                .shadow(color: .dropLight, radius: 15, x: -10, y: -10)
+                            
+                            }
+                            
+                            
+//                            Rating(item: items(id: self.item.id, item: self.item))
+                            
+                        }
+                        
+                        .padding(.horizontal, 20)
+                        .padding(.top, 10)
+                        
+                        
+                    
+                }
                 
                 Spacer()
                 
                 VStack{
-                    Text("\(price) \(ServerAPI.settings.symbol)")
+                    Text("\(self.item.discount) \(ServerAPI.settings.symbol)")
                         .foregroundColor(.secondary)
-                       
                     
-                    Text("\(quanity) / \(type) ")
+                    
+                    Text("\(self.item.quanity) / \(self.item.type) ")
                         .foregroundColor(.secondary)
-                       
+                    
                 } .padding(.horizontal, 10)
                 
-            
+                
                 
                 Image(systemName: "chevron.right")
             }.padding([.top, .bottom], 5)
