78b7660c6673b031ea13689937536ed6dafa0a9d
tinyprocessing <safir@thenoco.co>
2021-02-09 00:49:45 +0300

    get_basket

--- Yelm.ProjectX/realm/realm.swift
+++ Yelm.ProjectX/realm/realm.swift
@@ -17,7 +17,7 @@ var GlobalRealm: RealmControl = RealmControl()
 
 class RealmControl: ObservableObject, Identifiable {
     var id: Int = 0
-
+    
     @ObservedObject var cart: cart = GlobalCart
     @Published var price: Float = 0
     @Published var start_price: Float = 0
@@ -29,7 +29,7 @@ class RealmControl: ObservableObject, Identifiable {
             // Set the new schema version. This must be greater than the previously used
             // version (if you've never set a schema version before, the version is 0).
             schemaVersion: 2,
-
+            
             // Set the block which will be called automatically when opening a Realm with
             // a schema version lower than the one set above
             migrationBlock: { migration, oldSchemaVersion in
@@ -40,20 +40,20 @@ class RealmControl: ObservableObject, Identifiable {
                     // And will update the schema on disk automatically
                 }
             })
-
+        
         // Tell Realm to use this new configuration object for the default Realm
         Realm.Configuration.defaultConfiguration = config
-
+        
         // Now that we've told Realm how to handle the schema change, opening the file
         // will automatically perform the migration
-         realm = try! Realm()
+        realm = try! Realm()
     }
-
-  
     
-
+    
+    
+    
     func create_item_cart(ID: Int, Title: String, Price: Float, PriceItem: Float, Count: Int, Thumbnail: String, ItemType: String, Quantity: String, CanIncrement: String, Discount: Int = 0) {
-
+        
         let order = ItemRealm()
         order.ID = ID
         order.Title = Title
@@ -65,7 +65,7 @@ class RealmControl: ObservableObject, Identifiable {
         order.quantity = Quantity
         order.CanIncrement = CanIncrement
         order.Discount = Discount
-
+        
         try! realm.write {
             realm.add(order)
             get_total_price()
@@ -77,7 +77,7 @@ class RealmControl: ObservableObject, Identifiable {
             }
         }
     }
-
+    
     
     func get_ids() -> JSON {
         let realm = try! Realm()
@@ -91,86 +91,86 @@ class RealmControl: ObservableObject, Identifiable {
                 ]
                 
                 items.arrayObject?.append(item)
-
+                
                 
             }
             
             return items
-
+            
         }
         
         return JSON()
         
     }
-
+    
     func get_total_price() {
         
         let realm = try! Realm()
-
+        
         let objects = realm.objects(ItemRealm.self)
         
         var Price : Float = 0.0
         var Start_Price : Float = 0.0
-            if (objects.count > 0){
-                self.cart.cart_items.removeAll()
-                for i in 0...objects.count - 1 {
+        if (objects.count > 0){
+            self.cart.cart_items.removeAll()
+            for i in 0...objects.count - 1 {
+                
+                if (objects[i].Discount != 0){
                     
-                    if (objects[i].Discount != 0){
-                        
-                        let sym = Float(objects[i].Discount) / 100
-                        let disc = objects[i].Price * sym
-                        let discount_final = objects[i].Price - disc
-
-                        let final = discount_final
-                        
-                        Start_Price += objects[i].Price * Float(objects[i].Count)
-                        Price += (final * Float(objects[i].Count))
-                        
-                        let quantity = String(Int(objects[i].quantity)!*objects[i].Count)
-                        
-                        self.cart.cart_items.append(cart_structure(id: objects[i].ID,
-                                                                   image: objects[i].Thumbnail,
-                                                                   title: objects[i].Title,
-                                                                   price: "\(objects[i].Price)",
-                                                                   price_float: (final * Float(objects[i].Count)),
-                                                                   count: objects[i].Count,
-                                                                   type: objects[i].ItemType,
-                                                                   quantity: quantity))
-                        
-                      
-
-                    }else{
-                        Price += Float((Int(objects[i].Price) * objects[i].Count))
-                        Start_Price += objects[i].Price * Float(objects[i].Count)
-                        
-                        let quantity = String(Int(objects[i].quantity)!*objects[i].Count)
-
-                        self.cart.cart_items.append(cart_structure(id: objects[i].ID,
-                                                                   image: objects[i].Thumbnail,
-                                                                   title: objects[i].Title,
-                                                                   price: "\(objects[i].Price)",
-                                                                   price_float: Float((Int(objects[i].Price) * objects[i].Count)),
-                                                                   count: objects[i].Count,
-                                                                   type: objects[i].ItemType,
-                                                                   quantity: quantity))
+                    let sym = Float(objects[i].Discount) / 100
+                    let disc = objects[i].Price * sym
+                    let discount_final = objects[i].Price - disc
                     
-                    }
+                    let final = discount_final
+                    
+                    Start_Price += objects[i].Price * Float(objects[i].Count)
+                    Price += (final * Float(objects[i].Count))
+                    
+                    let quantity = String(Int(objects[i].quantity)!*objects[i].Count)
+                    
+                    self.cart.cart_items.append(cart_structure(id: objects[i].ID,
+                                                               image: objects[i].Thumbnail,
+                                                               title: objects[i].Title,
+                                                               price: "\(objects[i].Price)",
+                                                               price_float: (final * Float(objects[i].Count)),
+                                                               count: objects[i].Count,
+                                                               type: objects[i].ItemType,
+                                                               quantity: quantity))
+                    
+                    
+                    
+                }else{
+                    Price += Float((Int(objects[i].Price) * objects[i].Count))
+                    Start_Price += objects[i].Price * Float(objects[i].Count)
+                    
+                    let quantity = String(Int(objects[i].quantity)!*objects[i].Count)
+                    
+                    self.cart.cart_items.append(cart_structure(id: objects[i].ID,
+                                                               image: objects[i].Thumbnail,
+                                                               title: objects[i].Title,
+                                                               price: "\(objects[i].Price)",
+                                                               price_float: Float((Int(objects[i].Price) * objects[i].Count)),
+                                                               count: objects[i].Count,
+                                                               type: objects[i].ItemType,
+                                                               quantity: quantity))
+                    
+                }
             }
         }
         self.objectWillChange.send()
-   
-
+        
+        
         
         self.price = Price
         self.start_price = Start_Price
         
-     
+        
         
     }
     
- 
-  
-
+    
+    
+    
     func get_items_realmtype() -> Results<ItemRealm> {
         let realm = try! Realm()
         let objects = realm.objects(ItemRealm.self)
@@ -207,10 +207,10 @@ class RealmControl: ObservableObject, Identifiable {
             }
             
         }
-      
+        
         
     }
-
+    
     func delete_item(ID: Int) {
         let realm = try! Realm()
         let object = realm.objects(ItemRealm.self).filter("ID = \(ID)")
@@ -225,14 +225,14 @@ class RealmControl: ObservableObject, Identifiable {
             }
         }
     }
-
+    
     func get_item_realmtype(ID: Int) -> Results<ItemRealm> {
         let realm = try! Realm()
         let object = realm.objects(ItemRealm.self).filter("ID = \(ID)")
         return object
     }
-
-  
+    
+    
     func post_cart(ID: Int, method: String) {
         
         let realm = try! Realm()
@@ -255,7 +255,7 @@ class RealmControl: ObservableObject, Identifiable {
                     get_total_price()
                 }
                 
-              
+                
             }
             
         }
@@ -276,14 +276,50 @@ class RealmControl: ObservableObject, Identifiable {
                         
                         get_total_price()
                     }
-                   
+                    
                 }else{
                     self.delete_item(ID: ID)
-                  
+                    
                 }
             }
             
         }
+        
+        ServerAPI.basket.get_basket(items: self.get_ids()) { (load, removable)  in
+            if (load){
+                
+                removable.forEach { (item) in
+                    self.set_count(ID: item.item_id, count: item.count)
+                }
+                
+            }
+        }
+    }
+    
+    
+    func set_count(ID: Int, count: Int) {
+        
+        let realm = try! Realm()
+        let objects = realm.objects(ItemRealm.self).filter("ID = \(ID)")
+        
+        
+        if let object = objects.first {
+            try! realm.write {
+                object.Count = count
+                
+                ServerAPI.orders.set_temporary_orders(item_id: ID, action: "Превышение лимита", count: count) { (load) in
+                    if (load){
+                        YelmChat.core.send(message: "/basket", type: "basket")
+                    }
+                }
+                
+                get_total_price()
+            }
+            
+            
+        }
+        
+        
     }
     
     func get_items_count(ID: Int) -> Int {
@@ -300,12 +336,12 @@ class RealmControl: ObservableObject, Identifiable {
     func get_item_access(ID: Int) -> Bool {
         
         let show_items = self.get_items_count(ID: ID)
-
+        
         if show_items > 0{
             return true
         }else{
             return false
         }
     }
-
+    
 }
