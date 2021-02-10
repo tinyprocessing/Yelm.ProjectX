16eaeaa9c08a485c017f572921ab6ec0dd41529b
Michael Safir <admin@yelm.io>
2021-02-10 18:12:40 +0300

    strokeBorder stories

--- Yelm.ProjectX/pages/news.swift
+++ Yelm.ProjectX/pages/news.swift
@@ -14,6 +14,9 @@ struct News : View {
     @ObservedObject var news : news = GlobalNews
     @State var selection: Int? = nil
     
+    @State private var phase: CGFloat = 0
+    
+    
     var body: some View{
         VStack(spacing: 10){
             if (self.news.news.count > 0){
@@ -27,18 +30,37 @@ struct News : View {
                 ScrollView(.horizontal, showsIndicators: false){
                     HStack(spacing: 0){
                         ForEach(self.news.news, id: \.self){ object in
-                            NavigationLink(destination: NewsSingle()){
+                            NavigationLink(destination: story()){
                                 VStack{
-                                URLImage(URL(string: object.thubnail)!) { proxy in
-                                    proxy.image
-                                        .resizable()
-                                        .frame(width: proxy.news.width, height: proxy.news.height)
-                                        .aspectRatio(contentMode: .fill)
-                                        .cornerRadius(15)
-                                        .contentShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
-                                      
-                                }
-                            }.padding(.trailing, 15)
+                                    URLImage(URL(string: object.thubnail)!) { proxy in
+                                        proxy.image
+                                            .resizable()
+                                            .frame(width: proxy.news.width-5, height: proxy.news.height-5)
+                                            .aspectRatio(contentMode: .fill)
+                                            .cornerRadius(15)
+                                            .contentShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
+                                            .padding(5)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                                    .strokeBorder(style: StrokeStyle(
+                                                        lineWidth: 2,
+                                                        lineCap: .round,
+                                                        lineJoin: .round,
+                                                        dash: [10],
+                                                        dashPhase: self.phase
+                                                    ))
+                                                    .foregroundColor(Color.theme)
+                                                    .onAppear {
+                                                        self.phase = 0
+                                                        self.phase = -20
+                                                    }
+                                                    .animation(Animation.linear(duration: 0.5).repeatForever(autoreverses: false))
+
+                                            )
+                                        
+                                    }
+                                }.padding(.trailing, 15)
+                                
                             }.buttonStyle(ScaleButtonStyle())
                             
                             .simultaneousGesture(TapGesture().onEnded{
@@ -51,11 +73,11 @@ struct News : View {
                     }
                     .padding(.leading, 15)
                     .padding(.top, 15)
-                   
+                    
                 }.frame(height: 110)
                 
             }
-          
+            
         }
         .padding(.bottom, self.news.news.count > 0 ? 20 : 0)
         .onAppear{
