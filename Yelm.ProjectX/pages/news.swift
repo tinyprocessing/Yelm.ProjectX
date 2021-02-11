af79b7b852ce84a0e69e8562a9853644a0a88366
tinyprocessing <safir@thenoco.co>
2021-02-11 23:53:28 +0300

    news -> story -> server

--- Yelm.ProjectX/pages/news.swift
+++ Yelm.ProjectX/pages/news.swift
@@ -14,7 +14,7 @@ struct News : View {
     @ObservedObject var news : news = GlobalNews
     @State var selection: Int? = nil
     
-    @State private var phase: CGFloat = 0
+    @State private var phase: CGFloat = 1
     
     
     var body: some View{
@@ -25,63 +25,98 @@ struct News : View {
                         .font(.system(size: 26, weight: .bold, design: .rounded))
                     
                     Spacer()
-                }.padding(.leading, 15)
+                }
+                .padding(.leading, 15)
+                
                 
                 ScrollView(.horizontal, showsIndicators: false){
                     HStack(spacing: 0){
                         ForEach(self.news.news, id: \.self){ object in
-                            NavigationLink(destination: story()){
-                                VStack{
-                                    URLImage(URL(string: object.thubnail)!) { proxy in
-                                        proxy.image
-                                            .resizable()
-                                            .frame(width: proxy.news.width-5, height: proxy.news.height-5)
-                                            .aspectRatio(contentMode: .fill)
-                                            .cornerRadius(15)
-                                            .contentShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
-                                            .padding(5)
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 15, style: .continuous)
-                                                    .strokeBorder(style: StrokeStyle(
-                                                        lineWidth: 2,
-                                                        lineCap: .round,
-                                                        lineJoin: .round,
-                                                        dash: [10],
-                                                        dashPhase: self.phase
-                                                    ))
-                                                    .foregroundColor(Color.theme)
-                                                    .onAppear {
-                                                        self.phase = 0
-                                                        self.phase = -20
-                                                    }
-                                                    .animation(Animation.linear(duration: 0.5).repeatForever(autoreverses: false))
+                            if (object.type == "story"){
+                                NavigationLink(destination:  story(), tag: object.id, selection: $selection){
 
-                                            )
-                                        
-                                    }
-                                }.padding(.trailing, 15)
+                                    VStack{
+                                        URLImage(URL(string: object.thubnail)!) { proxy in
+                                            proxy.image
+                                                .resizable()
+                                                .frame(width: proxy.news.width-5, height: proxy.news.height-5)
+                                                .aspectRatio(contentMode: .fill)
+                                                .cornerRadius(15)
+                                                .contentShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
+                                                .padding(5)
+                                                .overlay(
+                                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                                        .strokeBorder(style: StrokeStyle(
+                                                            lineWidth: 2,
+                                                            lineCap: .round,
+                                                            lineJoin: .round,
+                                                            miterLimit: 0,
+                                                            dash: [10],
+                                                            dashPhase: self.phase
+                                                        ))
+                                                        .foregroundColor(Color.theme)
+                                                        .onAppear {
+                                                            self.phase = 1
+                                                            self.phase = -20
+                                                        }
+                                                        .animation(Animation.linear(duration: 0.5).repeatForever(autoreverses: false))
+
+                                                )
+
+                                        }
+                                    }.padding(.trailing, 10)
+
+                                }.buttonStyle(ScaleButtonStyle())
+//                                .background(Color.blue)
+
+                                .simultaneousGesture(TapGesture().onEnded{
+                                    print("story tapped")
+                                    let news = object
+                                    self.news.news_single = news
+                                })
+                            }
+                            
+                            if (object.type == "news"){
+                                NavigationLink(destination:  NewsSingle(), tag: object.id+1, selection: $selection ){
+                                    
+                                    VStack{
+                                        URLImage(URL(string: object.thubnail)!) { proxy in
+                                            proxy.image
+                                                .resizable()
+                                                .frame(width: proxy.news.width, height: proxy.news.height)
+                                                .aspectRatio(contentMode: .fill)
+                                                .cornerRadius(15)
+                                                .contentShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
+                                                .padding(5)
+
+                                        }
+                                    }.padding(.trailing, 10)
+                                    
+                                }.buttonStyle(ScaleButtonStyle())
+                               
                                 
-                            }.buttonStyle(ScaleButtonStyle())
-                            
-                            .simultaneousGesture(TapGesture().onEnded{
-                                let news = object
-                                self.news.news_single = news
-                            })
-                            
+                                .simultaneousGesture(TapGesture().onEnded{
+                                    print("news tapped")
+                                    let news = object
+                                    self.news.news_single = news
+                                })
+                            }
                             
                         }
                     }
+//                    HSTACK END
                     .padding(.leading, 15)
-                    .padding(.top, 15)
+                    .frame(height: 110)
+//                    .background(Color.green)
+//                    .padding(.top, 15)
                     
                 }.frame(height: 110)
+//                .background(Color.red)
                 
             }
             
         }
-        .padding(.bottom, self.news.news.count > 0 ? 20 : 0)
-        .onAppear{
-            
-        }
+        .padding(.bottom, self.news.news.count > 0 ? 10 : 0)
+      
     }
 }
