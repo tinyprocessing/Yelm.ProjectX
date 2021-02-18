23b35034ce1284da7085759965bf9d51c4d5e23f
tinyprocessing <safir@thenoco.co>
2021-02-18 18:30:45 +0300

    catalog prepare

---
+++ Yelm.ProjectX/pages/catalog.swift
@@ -0,0 +1,16 @@
+//
+//  catalog.swift
+//  Yelm.ProjectX
+//
+//  Created by Michael on 18.02.2021.
+//
+
+import SwiftUI
+
+struct catalog: View {
+    var body: some View {
+        Text("Catalog View!")
+    }
+}
+
+
