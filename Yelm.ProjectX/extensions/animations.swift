f8d8a71cb463347dd80653541a9b68cd24265be7
Michael Safir <admin@yelm.io>
2021-03-03 22:12:45 +0300

    animation

--- Yelm.ProjectX/extensions/animations.swift
+++ Yelm.ProjectX/extensions/animations.swift
@@ -14,22 +14,48 @@ var forever: Animation {
           .repeatForever(autoreverses: false)
 }
 
+var forever_interpolating: Animation {
+      Animation.interpolatingSpring(stiffness: 350, damping: 5, initialVelocity: 10)
+          .repeatForever(autoreverses: false)
+}
 
 struct LoaderEnot: View {
     
     @State private var isAnimating = false
-
+    @State private var show : Bool = false
     
     var body: some View {
         VStack{
+            if (self.show){
             Image("enot")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 80, height: 80)
                 .rotationEffect(Angle(degrees: self.isAnimating ? 360 : 0.0))
                 .animation(self.isAnimating ? .interpolatingSpring(mass: 1, stiffness: 1, damping: 0.7, initialVelocity: 2) : .default)
-                .onAppear { self.isAnimating = true }
-                .onDisappear { self.isAnimating = false }
+                .onAppear {
+                    withAnimation() {
+                        self.isAnimating = true
+                    }
+                    
+                }
+                .onDisappear {
+                    withAnimation() {
+                        self.isAnimating = false
+                    }
+                }
+            }else{
+                Image("enot")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 80, height: 80)
+            }
+        }.onAppear{
+            self.show = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
+                self.show = true
+            }
         }
     }
+  
 }
