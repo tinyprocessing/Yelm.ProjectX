75251999eccd07387c8bb95666382b3277fdec19
tinyprocessing <safir@thenoco.co>
2021-03-10 13:29:40 +0300

    pods

--- Yelm.ProjectX/Home.swift
+++ Yelm.ProjectX/Home.swift
@@ -312,11 +312,18 @@ struct Home: View {
             
             open_offer = false
             
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                AppStoreReviewManager.requestReviewIfAppropriate()
+            }
+            
             if(self.payment.payment_done){
                 self.payment.payment_done = false
                 
+                
+               
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                     ShowAlert(title: "Отлично", message: "Оплата прошла успешно - детали Вашего заказа отправлены в чат.")
+                    
                 }
 
             }
--- Yelm.ProjectX/Start.swift
+++ Yelm.ProjectX/Start.swift
@@ -273,6 +273,7 @@ struct Start: View {
                             }
                         }
                     }
+                    
                 }
             }
             
