61192063fd628a18cc9d7f3cfd4ea14d3e669415
tinyprocessing <safir@thenoco.co>
2021-04-08 12:42:40 +0300

    animations

--- Yelm.ProjectX/extensions/animations.swift
+++ Yelm.ProjectX/extensions/animations.swift
@@ -25,37 +25,79 @@ struct LoaderEnot: View {
     @State private var show : Bool = false
     
     var body: some View {
-        VStack{
-            if (self.show){
-            Image("enot")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 80, height: 80)
-                .rotationEffect(Angle(degrees: self.isAnimating ? 360 : 0.0))
-                .animation(self.isAnimating ? .interpolatingSpring(mass: 1, stiffness: 1, damping: 0.7, initialVelocity: 2) : .default)
-                .onAppear {
-                    withAnimation() {
-                        self.isAnimating = true
-                    }
-                    
-                }
-                .onDisappear {
-                    withAnimation() {
-                        self.isAnimating = false
-                    }
-                }
-            }else{
+        if (distribution == false){
+            VStack{
+                if (self.show){
                 Image("enot")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .frame(width: 80, height: 80)
+                    .rotationEffect(Angle(degrees: self.isAnimating ? 360 : 0.0))
+                    .animation(self.isAnimating ? .interpolatingSpring(mass: 1, stiffness: 1, damping: 0.7, initialVelocity: 2) : .default)
+                    .onAppear {
+                        withAnimation() {
+                            self.isAnimating = true
+                        }
+                        
+                    }
+                    .onDisappear {
+                        withAnimation() {
+                            self.isAnimating = false
+                        }
+                    }
+                }else{
+                    Image("enot")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 80, height: 80)
+                }
+            }.onAppear{
+                self.show = false
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
+                    self.show = true
+                }
             }
-        }.onAppear{
-            self.show = false
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
-                self.show = true
+        }else{
+            VStack{
+                
+                Image("start")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 80, height: 80)
+                
+                ActivityIndicator()
+                    .frame(CGSize(width: 60, height: 60))
+                    .foregroundColor(.theme)
+                
             }
         }
     }
   
+}
+
+
+struct ActivityIndicator: View {
+
+  @State private var isAnimating: Bool = false
+
+  var body: some View {
+    GeometryReader { (geometry: GeometryProxy) in
+      ForEach(0..<5) { index in
+        Group {
+          Circle()
+            .frame(width: geometry.size.width / 5, height: geometry.size.height / 5)
+            .scaleEffect(!self.isAnimating ? 1 - CGFloat(index) / 5 : 0.2 + CGFloat(index) / 5)
+            .offset(y: geometry.size.width / 10 - geometry.size.height / 2)
+          }.frame(width: geometry.size.width, height: geometry.size.height)
+            .rotationEffect(!self.isAnimating ? .degrees(0) : .degrees(360))
+            .animation(Animation
+              .timingCurve(0.5, 0.15 + Double(index) / 5, 0.25, 1, duration: 1.5)
+              .repeatForever(autoreverses: false))
+        }
+      }
+    .aspectRatio(1, contentMode: .fit)
+    .onAppear {
+        self.isAnimating = true
+    }
+  }
 }
