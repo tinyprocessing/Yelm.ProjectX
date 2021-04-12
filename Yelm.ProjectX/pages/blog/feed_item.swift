5ac5fa57e8b8b3801845b2d5044ae92f41c04016
tinyprocessing <safir@thenoco.co>
2021-04-12 11:34:56 +0300

    Feed_Item

--- Yelm.ProjectX/pages/blog/feed_item.swift
+++ Yelm.ProjectX/pages/blog/feed_item.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import SwiftUI
+import Yelm_Server
+
+
+
+struct Feed_Item: View {
+        
+    var body: some View{
+
+        VStack(alignment: .leading, spacing: 15){
+            
+            Image("feed_1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 200)
+                .cornerRadius(10)
+            
+            Text("Какие фрукты заказывать в апреле")
+            
+            Text("В апреле балуем вас экзотикой из жарких стран и потихоньку открываем новый ягодный сезон. Расскажем, откуда нам привозят самые вкусные и спелые фрукты для ваших заказов.")
+                .foregroundColor(.secondary)
+        }
+    }
+       
+}
